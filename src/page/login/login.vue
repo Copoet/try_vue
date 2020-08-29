3cb4b689b7d06202f91521fe2aceb3d69a10becf
copoet <copoet@126.com>
2020-08-29 21:01:54 +0800

    config

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -1,81 +1,95 @@
 <template>
-<div id="login-box">
-  <div class="box-group group-top">
-    <div class="site-img">
-      <h3><img src="@/assets/images/logo.png" alt="Logo" /></h3>
-    </div>
-  </div>
-  <div class="box-group">
-    <div class="controls">
-      <div class="controls-box">
-        <span class="add-on bg-lu">
+  <div id="login-body">
+    <div id="login-box">
+      <!-- <div class="box-group group-top">
+      <div class="site-img">
+        <h3><img src="@/assets/images/logo.png" alt="Logo" /></h3>
+      </div>
+      </div>-->
+      <div class="box-group">
+        <div class="controls">
+          <div class="controls-box">
+            <span class="add-on bg-lu">
               <i class="icon-user"></i>
             </span>
-        <el-input v-model="username" placeholder="Username"></el-input>
+            <el-input v-model="username" placeholder="请输入用户名"></el-input>
+          </div>
+        </div>
+        <div class="controls">
+          <div class="controls-box">
+            <span class="add-on bg-lg">
+              <i class="icon-lock"></i>
+            </span>
+            <el-input
+              placeholder="请输入密码"
+              v-model="pass"
+              type="password"
+              @keyup.enter.native="loginRequest"
+            ></el-input>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="box-group">
+        
+      </div> -->
+      <div class="box-group group-top">
+        <el-button type="success" class="login-btm" @click="loginRequest()">登录</el-button>
       </div>
     </div>
   </div>
-  <div class="box-group">
-    <div class="controls">
-      <div class="controls-box">
-        <span class="add-on bg-lg">
-            <i class="icon-lock"></i>
-          </span>
-        <el-input placeholder="Password" v-model="pass" type="password" @keyup.enter.native="loginRequest"></el-input>
-      </div>
-    </div>
-  </div>
-  <div class="box-group group-top">
-    <el-button type="success" class="login-btm" @click="loginRequest()">Login</el-button>
-  </div>
-</div>
 </template>
 <script>
-import api from '@/api/api'
+import api from "@/api/api";
 export default {
   data() {
     return {
-      username: '',
-      pass: ''
-    }
+      username: "",
+      pass: ""
+    };
   },
   methods: {
     loginRequest() {
-      if (this.username == '' || this.pass == '') {
+      if (this.username == "" || this.pass == "") {
         this.$notify.warning({
-          title: 'info',
-          message: '用户名密码不能为空',
+          title: "info",
+          message: "用户名密码不能为空"
         });
       } else {
         let data = {
           username: this.username,
           password: this.pass
-        }
+        };
         api.login(data).then(res => {
           if (res.data.code == 200) {
-            if(window.localStorage){
-              localStorage.setItem('token',JSON.stringify(res.data.list));
-            }else{
-              Cookie.write('token',res.data.list)
+            if (window.localStorage) {
+              localStorage.setItem("token", JSON.stringify(res.data.list));
+            } else {
+              Cookie.write("token", res.data.list);
             }
-            this.$router.replace('/home')
+            this.$router.replace("/home");
           }
-        })
+        });
       }
     }
   }
-}
+};
 </script>
 <style>
-html{
-   background: #2E363F;
+html {
+  /* background: #2E363F; */
+  background-image: url("../../assets/images/05.jpg");
+  background-size: cover;
+}
+#login-body {
+  /* background: #3f4954; */
+  height: 100%;
 }
 #login-box {
-  padding-top: 10%;
-  width: 32%;
-  margin-left: auto;
-  margin-right: auto;
-  position: relative;
+  padding-top: 20%;
+  width: 30%;
+  margin-left: 60%;
+  /* margin-right: 10%; */
+  /* position: relative; */
 }
 
 .site-img {
@@ -97,20 +111,20 @@ h3 img {
 }
 
 .controls input {
-  height: 41px;
   line-height: 30px;
   border-radius: 0px;
-  width: 86%;
+  width: 80%;
   display: block;
   float: right;
   position: absolute;
-  margin-top: -58px;
+  margin-top: -59px;
   margin-left: 48px;
+  height: 42px;
 }
 
 .add-on {
-  padding: 9px 9px;
-  height: 23px;
+  padding: 10px;
+  height: 22px;
   width: 30px;
   color: #fff;
   display: block;
@@ -121,13 +135,12 @@ h3 img {
 }
 
 .bg-lg {
-  background: #ffb848
+  background: #ffb848;
 }
 
 .group-top {
   margin-bottom: 0;
   background: none;
-  border-top: 1px solid #3f4954;
 }
 
 .login-btm {
@@ -135,4 +148,7 @@ h3 img {
   float: right;
   margin-top: 18px;
 }
+/* .el-input__inner{
+  border: 0px;
+} */
 </style>
